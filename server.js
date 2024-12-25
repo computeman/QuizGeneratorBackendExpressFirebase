d65b8a096c66cb6232b0c3ee15969c74a5c7d413
Daudi <daudimwanzia@gmail.com>
2024-12-25 12:25:25 +0300

    update cors

--- server.js
+++ server.js
@@ -9,6 +9,14 @@ import admin from "firebase-admin";
 dotenv.config();
 
 const app = express();
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://quiz-generator-gemini-ai.vercel.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); 
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+
+  next();
+});
 const corsOptions = {
   origin: "https://quiz-generator-gemini-ai.vercel.app",
   methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE", "OPTIONS"],
